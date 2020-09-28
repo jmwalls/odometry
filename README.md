18483f5bf7a033be695c1c904e51ea064b060862
Jeff Walls <jmwalls@umich.edu>
2020-09-27 17:58:04 -0700

    initial kitti utilities and notebook

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# odometry
+# Odometry Estimation
+
+We'd like to play around with different algorithms for estimating odometry from
+inertial (and perhaps other) sensors.
+
+TODO:
+
+* Add information about conda environment used to run/test...
+* Add how to run Jupyter notebooks...
---
+++ utils/data.py
@@ -0,0 +1,106 @@
+"""Utilities for loading data from publicly available data sets.
+"""
+import glob
+import os
+import urllib.request
+import zipfile
+
+import numpy as np
+import pandas as pd
+
+
+KITTI_URL = 'https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data'
+KITTI_OUT = 'data-cache'
+
+
+OXTS_COLUMNS = [
+    'lat',           # latitude of the oxts-unit (deg)
+    'lon',           # longitude of the oxts-unit (deg)
+    'alt',           # altitude of the oxts-unit (m)
+    'roll',          # roll angle (rad),    0 = level, positive = left side up,      range: -pi   .. +pi
+    'pitch',         # pitch angle (rad),   0 = level, positive = front down,        range: -pi/2 .. +pi/2
+    'yaw',           # heading (rad),       0 = east,  positive = counter clockwise, range: -pi   .. +pi
+    'vn',            # velocity towards north (m/s)
+    've',            # velocity towards east (m/s)
+    'vf',            # forward velocity, i.e. parallel to earth-surface (m/s)
+    'vl',            # leftward velocity, i.e. parallel to earth-surface (m/s)
+    'vu',            # upward velocity, i.e. perpendicular to earth-surface (m/s)
+    'ax',            # acceleration in x, i.e. in direction of vehicle front (m/s^2)
+    'ay',            # acceleration in y, i.e. in direction of vehicle left (m/s^2)
+    'az',            # acceleration in z, i.e. in direction of vehicle top (m/s^2)
+    'af',            # forward acceleration (m/s^2)
+    'al',            # leftward acceleration (m/s^2)
+    'au',            # upward acceleration (m/s^2)
+    'wx',            # angular rate around x (rad/s)
+    'wy',            # angular rate around y (rad/s)
+    'wz',            # angular rate around z (rad/s)
+    'wf',            # angular rate around forward axis (rad/s)
+    'wl',            # angular rate around leftward axis (rad/s)
+    'wu',            # angular rate around upward axis (rad/s)
+    'pos_accuracy',  # velocity accuracy (north/east in m)
+    'vel_accuracy',  # velocity accuracy (north/east in m/s)
+    'navstat',       # navigation status (see navstat_to_string)
+    'numsats',       # number of satellites tracked by primary GPS receiver
+    'posmode',       # position mode of primary GPS receiver (see gps_mode_to_string)
+    'velmode',       # velocity mode of primary GPS receiver (see gps_mode_to_string)
+    'orimode']       # orientation mode of primary GPS receiver (see gps_mode_to_string)
+
+
+def _parse_kitti_oxts(*, path):
+    # Load data one entry at a time...
+    files = sorted([f for f in glob.glob(os.path.join(path, 'data/*.txt'))])
+    data = np.array([np.fromfile(f, sep=' ') for f in files])
+
+    # Corresponding timestamps are in another file...
+    with open(os.path.join(path, 'timestamps.txt'), 'r') as f:
+        datestr = [l for l in f.readlines()]
+    index = pd.DatetimeIndex(data=datestr)
+    assert len(index) == data.shape[0]
+
+    # Create data frame ane augment with timestamp seconds column.
+    df = pd.DataFrame(data, index=index, columns=OXTS_COLUMNS)
+    df['timestamp'] = 1e-9 * index.astype(np.int64)
+    return df
+
+
+def get_kitti_data(*, drive, sequence, force_download=False):
+    """
+    Download raw odometry data from KITTI.
+
+    Note: we download the raw unsynced data since this includes 100Hz OXTS
+    outputs.
+
+    Parameters
+    ----------
+    drive : (str) drive string, e.g., 2011_09_26
+    sequence : (int) drive sequence number, e.g., 1
+    force_download : (bool) optionally require new download
+
+    Returns
+    -------
+    df :  pandas dataframe
+    """
+    os.makedirs(KITTI_OUT, exist_ok=True)
+    path = os.path.join(KITTI_OUT, drive)
+    if force_download or not os.path.exists(path):
+        def _download_extract(fin):
+            url = f'{KITTI_URL}/{fin}'
+            print(f'downloading {url}...')
+            fout, _ = urllib.request.urlretrieve(url)  # downloads to tmp
+            print(f'extracting {fout}...')
+            with zipfile.ZipFile(fout, 'r') as z:  # extracts to pwd
+                z.extractall(path=KITTI_OUT)
+        _download_extract(f'{drive}_calib.zip')
+        _download_extract(f'{drive}_drive_{sequence:04d}/{drive}_drive_{sequence:04d}_extract.zip')
+
+        # Remove image/velodyne data since we only care about oxts.
+        os.removedirs(os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'image_00'))
+        os.removedirs(os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'image_01'))
+        os.removedirs(os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'image_02'))
+        os.removedirs(os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'image_03'))
+        os.removedirs(os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'velodyne_points'))
+    assert os.path.exists(path)
+
+    df = _parse_kitti_oxts(
+            path=os.path.join(path, f'{drive}_drive_{sequence:04d}_extract', 'oxts'))
+    return df
